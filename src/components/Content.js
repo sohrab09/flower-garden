35b00d05fd134712d196a5cbee667e5e6cf515c5
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-06-11 16:16:21 +0600

    Done

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,19 +6,19 @@ const Content = () => {
   return (
     <>
       <div className='menu-card'>
-        <img src={ImageOne} alt='egg' className='h-full rounded mb-20 shadow' />
+        <img src={ImageOne} alt='red' className='h-full rounded mb-20 shadow' />
         <div className='center-content'>
           <h2 className='text-2xl mb-2'>Red Rose</h2>
           <p className='mb-2'>The symbol of Love</p>
-          <span>$16</span>
+          <span>$50</span>
         </div>
       </div>
       <div className='menu-card'>
-        <img src={ImageTwo} alt='egg' className='h-full rounded mb-20 shadow' />
+        <img src={ImageTwo} alt='yellow' className='h-full rounded mb-20 shadow' />
         <div className='center-content'>
           <h2 className='text-2xl mb-2'>Yellow Rose</h2>
           <p className='mb-2'>Positive feelings of warmth</p>
-          <span>$18</span>
+          <span>$40</span>
         </div>
       </div>
     </>
